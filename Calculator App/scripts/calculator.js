31db115968bcfd389d15ab8257cb86edc83a2255
Fumino <luigicucculliu8@gmail.com>
2024-11-14 15:40:26 +0100

    Add files via upload

---
+++ Calculator App/scripts/calculator.js
@@ -0,0 +1,34 @@
+let calculation = '';
+
+function updateCalc(value) {
+  calculation += value;
+
+  document.querySelector('.js-display')
+    .innerHTML = calculation;
+}
+
+function clearCalc() {
+  calculation = '';
+
+  document.querySelector('.js-display')
+    .innerHTML = '';
+}
+
+function backspace() {
+  calculation = document.querySelector('.js-display')
+    .innerHTML = calculation.toString().slice(0, -1);
+}
+
+function calculate() {
+  calculation = eval(calculation);
+
+  document.querySelector('.js-display')
+    .innerHTML = calculation.toFixed(3);
+}
+
+function surpriseNumber(first, last) {
+  const surprise = Math.floor(Math.random() * (last - first + 1) + first);
+
+  document.querySelector('.js-display')
+    .innerHTML = calculation += surprise;
+}
